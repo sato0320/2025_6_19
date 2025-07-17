d20ce6f0e96401e9d5a08594c7583c3bb85cd539
sato0320 <TTC2550SG0029@edu.tech.ac.jp>
2025-07-17 11:40:05 +0900

    ねじこのカメラ移動、ジャンプ動作の追加

--- Assets/Scripts/Nejikocontroller.cs
+++ Assets/Scripts/Nejikocontroller.cs
@@ -16,6 +16,14 @@ public class nejikocontroll : MonoBehaviour
 
     public float speed = 0f;
     Animator animator;
+
+    //ジャンプの高さを決める変数
+    public float jumpPower = 0f;
+
+    //重力の強さを決める変数
+
+    public float gravityPower = 0;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -25,8 +33,19 @@ public class nejikocontroll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(controller.isGrounded)
+        {
+            //Jumpキーの入力があればy軸に力をかける
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpPower;
+                animator.SetTrigger("jump");
+            }
+        }
+
         if(Input.GetAxis("Vertical") > 0.0f)
         {
+            //ねじこが前進する処理
             moveDirection.z = Input.GetAxis("Vertical") * speed;
         }
         else
@@ -37,17 +56,15 @@ public class nejikocontroll : MonoBehaviour
         //Horizontal(左右入力)があれば、ねじこを回転させる
         transform.Rotate(0, Input.GetAxis("Horizontal") * 3f, 0);
 
-        //Jumpキーの入力があればy軸に力をかける
-        if (Input.GetButton("Jump"))
-        {
-            moveDirection.y = 1f;
-            animator.SetTrigger("jump");
-        }
+        //キャラクターが重力で落下する処理
+        moveDirection.y = moveDirection.y - gravityPower * Time.deltaTime;
+
+       
 
         //移動量をTransformに変換する
         Vector3 globalDirection = transform.TransformDirection(moveDirection);
         //Controllerに移動量を移す
-        controller.Move(globalDirection);
+        controller.Move(globalDirection　* Time.deltaTime);
         //ねじこのアニメーションを最新する
         animator.SetBool("run", moveDirection.z > 0f);
 
---
+++ Assets/Scripts/playerrFollowcamera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerrFollowcamera : MonoBehaviour
+{
+    //カメラ位置をプレイヤーの位置に合わせて変更する
+
+    Vector3 CameraPosition = Vector3.zero;
+
+    public GameObject followTarget;
+
+    public float followSpeed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraPosition = followTarget.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position,
+            followTarget.transform.position - CameraPosition,
+            Time.deltaTime * followSpeed
+            );
+    }
+}
